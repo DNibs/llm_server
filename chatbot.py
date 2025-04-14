c5562f0a6fa50fd8c50109509472f99f0cc6f82a
DNibs <dnibs513@gmail.com>
2025-04-14 19:22:41 -0400

    implemented langgraph

--- chatbot.py
+++ chatbot.py
@@ -1,65 +1,85 @@
-import gradio as gr
-from langchain_community.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationChain
 import os
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END, MessagesState
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph.message import add_messages
+from langchain_core.messages import HumanMessage, AIMessage
+import gradio as gr
 
 # Set environment for OpenAI-compatible LM Studio endpoint
 os.environ["OPENAI_API_KEY"] = "lm-studio"  # dummy key; LM Studio doesn't require real auth
 os.environ["OPENAI_API_BASE"] = "http://localhost:1234/v1"  # LM Studio local server
 
 # Set up LangChain model (ChatOpenAI uses OpenAI-compatible endpoints)
-# TODO: The class `ChatOpenAI` was deprecated in LangChain 0.0.10 and will be removed in 1.0. An updated version of the class exists in the :class:`~langchain-openai package and should be used instead. To use it run `pip install -U :class:`~langchain-openai` and import as `from :class:`~langchain_openai import ChatOpenAI``.
 llm = ChatOpenAI(
     temperature=0.7,
     model_name="gemma-3-27b-it",  # name is arbitrary for local usage
 )
 
-# Add simple memory
-# TODO: LangChainDeprecationWarning: Please see the migration guide at: https://python.langchain.com/docs/versions/migrating_memory/
-memory = ConversationBufferMemory()
+# Initiate the graph
+workflow = StateGraph(state_schema=MessagesState)
 
-# Wrap in a ConversationChain
-# TODO: LangChainDeprecationWarning: The class `ConversationChain` was deprecated in LangChain 0.2.7 and will be removed in 1.0. Use :meth:`~RunnableWithMessageHistory: https:ce/core/runnabce/core/ce/corce/core/ce/core/ce/corce/core/runnables/langchain_core.runnables.history.RunnableWithMessageHistory.html` instead.
-conversation = ConversationChain(
-    llm=llm,
-    memory=memory,
-    verbose=False
-)
+# Define what happens in graph
+def call_model(state: MessagesState):
+    response = llm.invoke(state['messages'])
+    return {'messages': response}
 
-# Chat function
-def chat_fn(history, message):
-    response = conversation.predict(input=message)
-    history.append((message, response))
-    return history, ""
+# Define the single node in graph
+workflow.add_edge(START, 'model')
+workflow.add_node('model', call_model)
+
+# Add memory to graph
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+
+
+# Define the i/o between the graph and gradio
+def query_response(query, chat_history, config):
+    input_messages = [HumanMessage(query)]
+    output = app.invoke({"messages": input_messages}, config)
+    response = output["messages"][-1].content
+    chat_history.append({"role": "user", "content": query})
+    chat_history.append({"role": "assistant", "content": response})   
+    return chat_history, '' # clears input box
+
+
+def clear_fn(config):
+    config["configurable"]["thread_id"] = str(int(config["configurable"]["thread_id"]) + 1)  # increment thread id
+    return [], '', config  # return updated state
+
+
+# Sets unique value for each thread; change to clear history
+config_id = {"configurable": {"thread_id": "1"}}
 
 # Gradio UI
 with gr.Blocks() as demo:
-    gr.Markdown("### 🤖 Local LLM Chatbot via LangChain + LM Studio")
-    #chatbot = gr.Chatbot(type='messages')
-    #TODO: deprecated, update to line above, but currently gives errors 
-    chatbot = gr.Chatbot()
-    state = gr.State([])
+    gr.Markdown("### 🤖 Local LLM Chatbot via LangChain + LM Studio") 
+    chat_history = gr.Chatbot(type="messages")
+    config_state = gr.State(config_id)
 
     with gr.Row():
-        msg = gr.Textbox(
-            show_label=False,
-            placeholder="Type your message...",
-            lines=1,
-            scale=8
-        )
+        msg = gr.Textbox('Type your message here!')
         send = gr.Button("Send", scale=1)
 
-    clear = gr.Button("Clear")
+    clear = gr.Button("Clear History and Starts New Thread", scale=1)
 
-    # Submit with Enter
-    msg.submit(chat_fn, [state, msg], [chatbot, msg])
+    msg.submit(
+        query_response, 
+        inputs=[msg, chat_history, config_state], 
+        outputs=[chat_history, msg],
+        )
+    
+    send.click(
+        query_response, 
+        inputs=[msg, chat_history, config_state], 
+        outputs=[chat_history, msg],
+        )
 
-    # Submit with Send button
-    send.click(chat_fn, [state, msg], [chatbot, msg])
+    clear.click(
+        clear_fn,
+        inputs=[config_state],
+        outputs=[chat_history, msg, config_state],
+        )
 
-    # Clear chat
-    # TODO: Make this clear chat history. Currently, it clears only visual.
-    clear.click(lambda: ([], ""), None, [chatbot, msg])
 
 demo.launch(server_name='0.0.0.0', server_port=7860, share=False)
